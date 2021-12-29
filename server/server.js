bba340d2031f4305c91dc2ec6a4f5b046622c26d
jeremy-cady <jeremy.cady@outlook.com>
2021-12-28 21:07:04 -0600

    server setup complete

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
 console.log('in server.js');
+
+// require express
+const express = require('express');
+
+// require body-parser
+const bodyParser = require('body-parser');
+
+// setup express app
+const app = express();
+const port = 5000;
+
+// point express to public files
+app.use(express.static('server/public'));
+
+// setup body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+
+// server listening
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+    
+})
